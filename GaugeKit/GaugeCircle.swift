7cd1af44193fc82f254fdc7ab3132eb97cab82d5
Petr Korolev <sky4winder@gmail.com>
2015-06-22 12:20:53 +0300

    add gradient to circle

--- GaugeKit/GaugeCircle.swift
+++ GaugeKit/GaugeCircle.swift
@@ -12,6 +12,9 @@ import QuartzCore
 @IBDesignable
 public class GaugeCircle: Gauge {
 
+    /// ring gradient layer
+    var ringGradientLayer2: CAGradientLayer!
+
     override public func layoutSubviews() {
 
         super.layoutSubviews()
@@ -31,7 +34,21 @@ public class GaugeCircle: Gauge {
             ringLayer = CAShapeLayer.getOval(lineWidth, path: nil, strokeStart: 0, strokeEnd: 1, strokeColor: UIColor.clearColor(), fillColor: UIColor.clearColor(), shadowRadius: shadowRadius, shadowOpacity: shadowOpacity, shadowOffsset: CGSizeZero, bounds: bounds)
 
             ringLayer.frame = layer.bounds
-            gaugeLayer.addSublayer(ringLayer)
+//            gaugeLayer.addSublayer(ringLayer)
+        }
+
+        if ringGradientLayer == nil {
+            ringGradientLayer = CAGradientLayer()
+            let adjust: CGFloat = abs((layer.bounds.height - layer.bounds.width) / 2 / layer.bounds.height)
+            ringGradientLayer.startPoint = CGPointMake(0, 0.5)
+            ringGradientLayer.endPoint = CGPointMake(1, 0.5)
+//            if layer.bounds.width < layer.bounds.height {
+//            } else {
+//            }
+            ringGradientLayer.colors = [startColor.CGColor, _endColor.CGColor]
+            ringGradientLayer.frame = layer.bounds
+            ringGradientLayer.mask = ringLayer
+            gaugeLayer.addSublayer(ringGradientLayer)
         }
         gaugeLayer.frame = layer.bounds
         gaugeLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
